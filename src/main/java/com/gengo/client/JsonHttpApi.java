b57e9019e09a77f584bb6bee2269fb68cfdfff48
Shawn Smith <shawnpsmith@gmail.com>
2012-11-26 16:23:20 +0900

    tabs to spaces, remove trailing whitespace

--- src/main/java/com/gengo/client/JsonHttpApi.java
+++ src/main/java/com/gengo/client/JsonHttpApi.java
@@ -34,74 +34,74 @@ import com.gengo.client.exceptions.GengoException;
  */
 public class JsonHttpApi
 {
-	public static final String CLIENT_VERSION = "2.0";
-	
-	/** Whether authentication is required by default for API method calls. */
-	private static final boolean AUTHENTICATION_REQUIRED_DEFAULT = true;
-	
-	protected String publicKey;
-	protected String privateKey;
+    public static final String CLIENT_VERSION = "2.0";
 
-	/**
-	 * Initialize a new API instance with the specified keys.
-	 * @param publicKey Public API key
-	 * @param privateKey Private API key
-	 */
-	public JsonHttpApi(String publicKey, String privateKey)
-	{
-		this.publicKey = publicKey;
-		this.privateKey = privateKey;
-	}
-	
-	/**
-	 * @return A timestamp string representing the current time.
-	 */
+    /** Whether authentication is required by default for API method calls. */
+    private static final boolean AUTHENTICATION_REQUIRED_DEFAULT = true;
+
+    protected String publicKey;
+    protected String privateKey;
+
+    /**
+     * Initialize a new API instance with the specified keys.
+     * @param publicKey Public API key
+     * @param privateKey Private API key
+     */
+    public JsonHttpApi(String publicKey, String privateKey)
+    {
+        this.publicKey = publicKey;
+        this.privateKey = privateKey;
+    }
+
+    /**
+     * @return A timestamp string representing the current time.
+     */
     private String getTimestamp()
     {
         Calendar cal = Calendar.getInstance();
         //cal.add(Calendar.MILLISECOND, -cal.get(Calendar.DST_OFFSET) - cal.get(Calendar.ZONE_OFFSET));
         return String.valueOf(cal.getTimeInMillis() / 1000);
     }
-	
+
     /**
      * @param url The API method URL
      * @param method An HTTP method
      * @return A JSONObject containing the response from the server
      * @throws GengoException if something went wrong :(
      */
-	protected JSONObject call(String url, HttpMethod method) throws GengoException
-	{
-		return call(url, method, null, AUTHENTICATION_REQUIRED_DEFAULT);
-	}
+    protected JSONObject call(String url, HttpMethod method) throws GengoException
+    {
+        return call(url, method, null, AUTHENTICATION_REQUIRED_DEFAULT);
+    }
 
-	/**
+    /**
      * @param url The API method URL
      * @param method An HTTP method
      * @param data JSON data to send in request
      * @return A JSONObject containing the response from the server
      * @throws GengoException if something went wrong :(
-	 */
-	protected JSONObject call(String url, HttpMethod method, JSONObject data) throws GengoException
-	{
-		return call(url, method, data, AUTHENTICATION_REQUIRED_DEFAULT);
-	}
-	
-	/**
+     */
+    protected JSONObject call(String url, HttpMethod method, JSONObject data) throws GengoException
+    {
+        return call(url, method, data, AUTHENTICATION_REQUIRED_DEFAULT);
+    }
+
+    /**
      * @param url The API method URL
      * @param method An HTTP method
      * @param requiresAuthentication true iff authentication is required for this call
      * @return A JSONObject containing the response from the server
      * @throws GengoException if something went wrong :(
-	 */
-	protected JSONObject call(String url, HttpMethod method, boolean requiresAuthentication) throws GengoException
-	{
-		return call(url, method, null, requiresAuthentication);
-	}
+     */
+    protected JSONObject call(String url, HttpMethod method, boolean requiresAuthentication) throws GengoException
+    {
+        return call(url, method, null, requiresAuthentication);
+    }
 
-	private void handleData(Map<String, String> parameters, JSONObject data, HttpMethod method)
-	{
-	    if (null != data)
-	    {
+    private void handleData(Map<String, String> parameters, JSONObject data, HttpMethod method)
+    {
+        if (null != data)
+        {
             if (HttpMethod.GET == method)
             {
                 @SuppressWarnings("rawtypes") // This flows in from JSONJava
@@ -122,82 +122,82 @@ public class JsonHttpApi
             {
                 parameters.put("data", data.toString());
             }
-	    }
-	}
-        
-	/**
+        }
+    }
+
+    /**
      * @param url The API method URL
      * @param method An HTTP method
      * @param data JSON data to send in request
      * @param requiresAuthentication true iff authentication is required for this call
      * @return A JSONObject containing the response from the server
      * @throws GengoException if something went wrong :(
-	 */
-	protected JSONObject call(String url, HttpMethod method, JSONObject data, boolean requiresAuthentication) throws GengoException
-	{
-		if (requiresAuthentication && (null == this.publicKey) || (null == this.privateKey))
-		{
-			throw new GengoException("This API requires authentication. Both a public and private key must be specified.");
-		}
+     */
+    protected JSONObject call(String url, HttpMethod method, JSONObject data, boolean requiresAuthentication) throws GengoException
+    {
+        if (requiresAuthentication && (null == this.publicKey) || (null == this.privateKey))
+        {
+            throw new GengoException("This API requires authentication. Both a public and private key must be specified.");
+        }
 
-		Map<String, String> parameters = new HashMap<String, String>();
-		parameters.put("api_key", this.publicKey);
+        Map<String, String> parameters = new HashMap<String, String>();
+        parameters.put("api_key", this.publicKey);
 
-		if (requiresAuthentication)
-		{
-			parameters.put("ts", getTimestamp());
-			parameters.put("api_sig", hmac(parameters.get("ts")));
-		}
-	    handleData(parameters, data, method);
+        if (requiresAuthentication)
+        {
+            parameters.put("ts", getTimestamp());
+            parameters.put("api_sig", hmac(parameters.get("ts")));
+        }
+        handleData(parameters, data, method);
 
-		String queryString = makeQueryString(parameters);
-		String rsp = sendRequest(method, url, queryString);
+        String queryString = makeQueryString(parameters);
+        String rsp = sendRequest(method, url, queryString);
 
-		JSONObject doc;
-		try
-		{
-			doc = new JSONObject(rsp);
-			if ("error".equals(doc.getString("opstat")))
-			{
-				JSONObject error = doc.getJSONObject("err");
-				throw new ErrorResponseException(error.getString("msg"), error.getInt("code"));
-			}
-		}
-		catch (JSONException e)
-		{
-			throw new GengoException("The response from the server is not valid JSON: " + rsp, e);
-		}
-		return doc;
-	}
+        JSONObject doc;
+        try
+        {
+            doc = new JSONObject(rsp);
+            if ("error".equals(doc.getString("opstat")))
+            {
+                JSONObject error = doc.getJSONObject("err");
+                throw new ErrorResponseException(error.getString("msg"), error.getInt("code"));
+            }
+        }
+        catch (JSONException e)
+        {
+            throw new GengoException("The response from the server is not valid JSON: " + rsp, e);
+        }
+        return doc;
+    }
 
-	/**
-	 * Builds a query string from a dictionary of parameters and values
-	 * @param data Maps parameters to values
-	 * @return A properly encoded URL query string
-	 * @throws GengoException
-	 */
-	private String makeQueryString(Map<String, String> data) throws GengoException
-	{
-		StringBuffer sb = new StringBuffer();
-		for(Map.Entry<String, String> e : data.entrySet())
-		{
-			try {
-				sb.append(String.format(
-						"&%s=%s",
-						URLEncoder.encode(e.getKey(), "UTF-8"),
-						URLEncoder.encode(e.getValue(), "UTF-8")
-				));
-			}
-			catch (UnsupportedEncodingException ex)
-			{
-				throw new GengoException("URLEncoder does not support UTF-8", ex);
-			}
-		}
-		//sb.deleteCharAt(0); // Remove the initial ampersand
+    /**
+     * Builds a query string from a dictionary of parameters and values
+     * @param data Maps parameters to values
+     * @return A properly encoded URL query string
+     * @throws GengoException
+     */
+    private String makeQueryString(Map<String, String> data) throws GengoException
+    {
+        StringBuffer sb = new StringBuffer();
+        for(Map.Entry<String, String> e : data.entrySet())
+        {
+            try {
+                sb.append(String.format(
+                        "&%s=%s",
+                        URLEncoder.encode(e.getKey(), "UTF-8"),
+                        URLEncoder.encode(e.getValue(), "UTF-8")
+                ));
+            }
+            catch (UnsupportedEncodingException ex)
+            {
+                throw new GengoException("URLEncoder does not support UTF-8", ex);
+            }
+        }
+        //sb.deleteCharAt(0); // Remove the initial ampersand
 
-		return sb.toString();
-	}
-	
+        return sb.toString();
+    }
+
     /**
      * Create a signature for data
      * Note: both data and private_key are assumed to be ISO-8859-1
@@ -230,7 +230,7 @@ public class JsonHttpApi
         {
             throw new GengoException("Signing failed", ex);
         }
-    }	
+    }
 
     /**
      * Perform an HTTP POST
@@ -241,36 +241,36 @@ public class JsonHttpApi
      */
     private String httpPost(HttpURLConnection con, String query) throws GengoException
     {
-		con.setDoOutput(true);
-		con.setRequestProperty("Content-Type", "application/x-www-form-urlencoded;charset=UTF-8");
-		try
-		{
-			String length = Integer.toString(query.getBytes("UTF-8").length);
-	        con.setRequestProperty("Content-Length", length);
-	        BufferedWriter out = new BufferedWriter(new OutputStreamWriter(con.getOutputStream()));
-	        out.write(query);
-	        out.flush();
-	        out.close();
-	        BufferedReader reader = new BufferedReader(new InputStreamReader(con.getInputStream(), Charset.forName("UTF-8")));
-	        String line;
-	        StringBuffer buf = new StringBuffer();
-	        while (null != (line = reader.readLine()))
-	        {
-	            buf.append(line);
-	        }
-	        if (HttpURLConnection.HTTP_OK != con.getResponseCode()
-	        	&& HttpURLConnection.HTTP_CREATED != con.getResponseCode())
-	        {
-	        	throw new GengoException(String.format("Unexpected HTTP response: %d", con.getResponseCode()));
-	        }
-	        return buf.toString();
-		}
-		catch (Exception e)
-		{
-			throw new GengoException("HTTP POST failed", e);
-		}       
+        con.setDoOutput(true);
+        con.setRequestProperty("Content-Type", "application/x-www-form-urlencoded;charset=UTF-8");
+        try
+        {
+            String length = Integer.toString(query.getBytes("UTF-8").length);
+            con.setRequestProperty("Content-Length", length);
+            BufferedWriter out = new BufferedWriter(new OutputStreamWriter(con.getOutputStream()));
+            out.write(query);
+            out.flush();
+            out.close();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(con.getInputStream(), Charset.forName("UTF-8")));
+            String line;
+            StringBuffer buf = new StringBuffer();
+            while (null != (line = reader.readLine()))
+            {
+                buf.append(line);
+            }
+            if (HttpURLConnection.HTTP_OK != con.getResponseCode()
+                && HttpURLConnection.HTTP_CREATED != con.getResponseCode())
+            {
+                throw new GengoException(String.format("Unexpected HTTP response: %d", con.getResponseCode()));
+            }
+            return buf.toString();
+        }
+        catch (Exception e)
+        {
+            throw new GengoException("HTTP POST failed", e);
+        }
     }
-    
+
     /**
      * Perform an HTTP GET
      * @param con An open, configured HTTP connection
@@ -279,27 +279,27 @@ public class JsonHttpApi
      */
     private String httpGet(HttpURLConnection con) throws GengoException
     {
-		try
-		{
-	        BufferedReader reader = new BufferedReader(new InputStreamReader(con.getInputStream(), Charset.forName("UTF-8")));
-	        String line;
-	        StringBuffer buf = new StringBuffer();
-	        while (null != (line = reader.readLine()))
-	        {
-	            buf.append(line);
-	        }
-	        if (HttpURLConnection.HTTP_OK != con.getResponseCode())
-	        {
-	        	throw new GengoException(String.format("Unexpected HTTP response: %i", con.getResponseCode()));
-	        }
-	        return buf.toString();
-		}
-		catch (Exception e)
-		{
-			throw new GengoException("HTTP GET failed", e);
-		}
+        try
+        {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(con.getInputStream(), Charset.forName("UTF-8")));
+            String line;
+            StringBuffer buf = new StringBuffer();
+            while (null != (line = reader.readLine()))
+            {
+                buf.append(line);
+            }
+            if (HttpURLConnection.HTTP_OK != con.getResponseCode())
+            {
+                throw new GengoException(String.format("Unexpected HTTP response: %i", con.getResponseCode()));
+            }
+            return buf.toString();
+        }
+        catch (Exception e)
+        {
+            throw new GengoException("HTTP GET failed", e);
+        }
     }
-    
+
     /**
      * Perform an HTTP DELETE
      * @param con An open, configured HTTP connection
@@ -328,36 +328,36 @@ public class JsonHttpApi
             throw new GengoException("HTTP DELETE failed", e);
         }
     }
-    
+
     private String httpPut(HttpURLConnection con, String query) throws GengoException
     {
-		con.setDoOutput(true);
-		con.setRequestProperty("Content-Type", "text/plain");
-		try
-		{
-			String length = Integer.toString(query.getBytes("UTF-8").length);
-	        con.setRequestProperty("Content-Length", length);
-	        DataOutputStream out = new DataOutputStream(con.getOutputStream());
-	        out.writeBytes(query);
-	        out.flush();
-	        out.close();
-	        BufferedReader reader = new BufferedReader(new InputStreamReader(con.getInputStream(), Charset.forName("UTF-8")));
-	        String line;
-	        StringBuffer buf = new StringBuffer();
-	        while (null != (line = reader.readLine()))
-	        {
-	            buf.append(line);
-	        }
-	        if (HttpURLConnection.HTTP_OK != con.getResponseCode())
-	        {
-	        	throw new GengoException(String.format("Unexpected HTTP response: %i", con.getResponseCode()));
-	        }
-	        return buf.toString();
-		}
-		catch (Exception e)
-		{
-			throw new GengoException("HTTP PUT failed", e);
-		}
+        con.setDoOutput(true);
+        con.setRequestProperty("Content-Type", "text/plain");
+        try
+        {
+            String length = Integer.toString(query.getBytes("UTF-8").length);
+            con.setRequestProperty("Content-Length", length);
+            DataOutputStream out = new DataOutputStream(con.getOutputStream());
+            out.writeBytes(query);
+            out.flush();
+            out.close();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(con.getInputStream(), Charset.forName("UTF-8")));
+            String line;
+            StringBuffer buf = new StringBuffer();
+            while (null != (line = reader.readLine()))
+            {
+                buf.append(line);
+            }
+            if (HttpURLConnection.HTTP_OK != con.getResponseCode())
+            {
+                throw new GengoException(String.format("Unexpected HTTP response: %i", con.getResponseCode()));
+            }
+            return buf.toString();
+        }
+        catch (Exception e)
+        {
+            throw new GengoException("HTTP PUT failed", e);
+        }
     }
 
 
@@ -371,28 +371,28 @@ public class JsonHttpApi
      */
     private HttpURLConnection createHttpConnection(HttpMethod method, String url, String queryString) throws GengoException
     {
-    	HttpURLConnection con;
-    	try
-    	{
-    		if (HttpMethod.GET == method || HttpMethod.DELETE == method)
-    		{
-    			url += "?" + queryString;
-    		}
-			URL u = new URL(url);
-			con = (HttpURLConnection)u.openConnection();
-			con.setRequestMethod(method.toString());
-			con.setDoInput(true);
-	        con.setUseCaches(false);
-	        con.setRequestProperty("User-Agent", "Gengo Java library; Version " + CLIENT_VERSION + "; http://gengo.com/");
-	        con.setRequestProperty("accept", "application/json");
-    	}
-    	catch (Exception e)
-    	{
-    		throw new GengoException("Failed to create HTTP connection", e);
-    	}		
-    	return con;
+        HttpURLConnection con;
+        try
+        {
+            if (HttpMethod.GET == method || HttpMethod.DELETE == method)
+            {
+                url += "?" + queryString;
+            }
+            URL u = new URL(url);
+            con = (HttpURLConnection)u.openConnection();
+            con.setRequestMethod(method.toString());
+            con.setDoInput(true);
+            con.setUseCaches(false);
+            con.setRequestProperty("User-Agent", "Gengo Java library; Version " + CLIENT_VERSION + "; http://gengo.com/");
+            con.setRequestProperty("accept", "application/json");
+        }
+        catch (Exception e)
+        {
+            throw new GengoException("Failed to create HTTP connection", e);
+        }
+        return con;
     }
-    
+
     /**
      * Send an HTTP request
      * @param method The HTTP method to use
@@ -401,33 +401,33 @@ public class JsonHttpApi
      * @return The response from the server
      * @throws GengoException
      */
-	private String sendRequest(HttpMethod method, String url, String queryString) throws GengoException
-	{
-		HttpURLConnection con = createHttpConnection(method, url, queryString);
-		switch (method)
-		{
-		case GET:
-			con.setRequestProperty("Content-Length", "0");
-			return httpGet(con);
-		case PUT:
-			return httpPut(con, queryString);
-		case POST:
-			return httpPost(con, queryString);
-		case DELETE:
-		    return httpDelete(con);
-		default:
-			throw new GengoException("HTTP method " + method.toString() + " is not supported");
-		}
-	}
-	
-	/**
-	 * Get an image from a specified URL with authentication and timestamp.
-	 * @param url The URL to which authentication and timestamp parameters will be added.
-	 * @return an image downloaded from the specified URL
-	 * @throws GengoException
-	 */
-	protected BufferedImage getImage(String url) throws GengoException
-	{
+    private String sendRequest(HttpMethod method, String url, String queryString) throws GengoException
+    {
+        HttpURLConnection con = createHttpConnection(method, url, queryString);
+        switch (method)
+        {
+        case GET:
+            con.setRequestProperty("Content-Length", "0");
+            return httpGet(con);
+        case PUT:
+            return httpPut(con, queryString);
+        case POST:
+            return httpPost(con, queryString);
+        case DELETE:
+            return httpDelete(con);
+        default:
+            throw new GengoException("HTTP method " + method.toString() + " is not supported");
+        }
+    }
+
+    /**
+     * Get an image from a specified URL with authentication and timestamp.
+     * @param url The URL to which authentication and timestamp parameters will be added.
+     * @return an image downloaded from the specified URL
+     * @throws GengoException
+     */
+    protected BufferedImage getImage(String url) throws GengoException
+    {
         Map<String, String> parameters = new HashMap<String, String>();
         parameters.put("api_key", this.publicKey);
         parameters.put("ts", getTimestamp());
@@ -448,5 +448,5 @@ public class JsonHttpApi
             call(url, HttpMethod.GET);
             throw new GengoException("IO Exception loading image", e);
         }
-	}
+    }
 }
